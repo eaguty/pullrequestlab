1e7a403eea54422bc0deb787f45da45c225a036c
eagutierrezp <60113023+eagutierrezp@users.noreply.github.com>
2022-11-02 14:24:12 -0600

    Se añade nuevo cambio

--- misaludo.py
+++ misaludo.py
@@ -3,3 +3,9 @@ nombre = "Eynar Gutierrez"
 edad = 28
 print("Mi nombre es {} y tengo una edad de {}").format(nombre,edad)
 #print("Mi nombre es {} y tengo una edad de {}").format(nombre,edad)
+
+
+numero1=10
+numero2=20
+suma=numero1+numero2
+print(suma)
